b00dc35cbbdd27312d0d04941db6054b155a9ba1
Abdullokh Musaev <abdulloh.m@payme.uz>
2024-07-13 14:59:33 +0500

    pass env var to lambda from cdk stack

--- authorization-service/authorization-service.go
+++ authorization-service/authorization-service.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"log"
+	"os"
+
 	"github.com/aws/aws-cdk-go/awscdk/v2"
 	"github.com/aws/aws-cdk-go/awscdk/v2/awslambda"
+	"github.com/joho/godotenv"
 
 	"github.com/aws/constructs-go/constructs/v10"
 	"github.com/aws/jsii-runtime-go"
@@ -10,6 +14,7 @@ import (
 
 type AuthorizationServiceStackProps struct {
 	awscdk.StackProps
+	SecretKey string
 }
 
 func NewAuthorizationServiceStack(scope constructs.Construct, id string, props *AuthorizationServiceStackProps) awscdk.Stack {
@@ -20,11 +25,14 @@ func NewAuthorizationServiceStack(scope constructs.Construct, id string, props *
 	stack := awscdk.NewStack(scope, &id, &sprops)
 
 	// * lambda handlers
-	basicAuthorizer := awslambda.NewFunction(stack, jsii.String("GetProductsHandler"), &awslambda.FunctionProps{
-		FunctionName: jsii.String("basicAuthorizer"),
+	basicAuthorizer := awslambda.NewFunction(stack, jsii.String("BasicAuthorizer"), &awslambda.FunctionProps{
+		FunctionName: jsii.String("BasicAuthorizer"),
 		Code:         awslambda.Code_FromAsset(jsii.String("handlers"), nil),
 		Runtime:      awslambda.Runtime_NODEJS_18_X(),
 		Handler:      jsii.String("basicAuthorizer.handler"),
+		Environment: &map[string]*string{
+			"SECRET_KEY": &props.SecretKey,
+		},
 	})
 
 	// Output the function name to use in other service cdk stack name
@@ -36,11 +44,21 @@ func NewAuthorizationServiceStack(scope constructs.Construct, id string, props *
 }
 
 func main() {
+	// Load environment variables from .env file
+	err := godotenv.Load("../.env")
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+
 	defer jsii.Close()
+
+	secretKey := os.Getenv("SECRET_KEY")
 
 	app := awscdk.NewApp(nil)
 
-	NewAuthorizationServiceStack(app, "ImportServiceStack", &AuthorizationServiceStackProps{})
+	NewAuthorizationServiceStack(app, "ImportServiceStack", &AuthorizationServiceStackProps{
+		SecretKey: secretKey,
+	})
 
 	app.Synth(nil)
 }
